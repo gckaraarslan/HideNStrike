93a37a72c69d5d0fb39df4f5b0df3c0b41915d40
Galip Can KARAARSLAN <gckaraarslan@gmail.com>
2024-05-17 12:30:09 +0300

    Recoil

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -25,10 +25,34 @@ public class Weapon : MonoBehaviour
  public Animation animation;
 
  public AnimationClip reload;
+
+ [Header("Recoil Settings")]
+ // [Range(0, 1)]
+ // public float recoilPercent = 0.3f;
+
+ [Range(0, 2)] public float recoverPercent = 0.7f;
+
+ [Space] 
+ public float recoilUp = 1f;
+ public float recoilBack = 0f;
+
+ private Vector3 originalPosition;
+ private Vector3 recoilVelocity = Vector3.zero;
+
+ private float recoilLength;
+ private float recoverLength;
+
+ private bool recoiling;
+ public bool recovering;
 private void Start()
  {
   magText.text = mag.ToString();
   ammoText.text = ammo + "/" + magAmmo;
+
+  originalPosition = transform.localPosition;
+
+  recoilLength = 0;
+  recoverLength = 1 / firePerSeconds * recoverPercent;
  }
 
  private void Update()
@@ -38,7 +62,7 @@ private void Start()
    nextFireMoment -= Time.deltaTime;
   }
 
-  if (Input.GetButton("Fire1") && nextFireMoment<=0 && ammo>0 && animation.isPlaying==false)
+  if (Input.GetButton("Fire1") && nextFireMoment<=0 && ammo>0 && animation.isPlaying==false )
   {
    nextFireMoment = 1 / firePerSeconds;
    ammo--;
@@ -48,9 +72,19 @@ private void Start()
    
   }
 
-  if (Input.GetKeyDown(KeyCode.R))
+  if (Input.GetKeyDown(KeyCode.R) && mag>0)
   {
    Reload();
+  }
+
+  if (recoiling)
+  {
+   Recoil();
+  }
+
+  if (recovering)
+  {
+   Recover();
   }
  }
 
@@ -68,7 +102,8 @@ private void Start()
 
  void Fire()
  {
-
+  recoiling = true;
+  recovering = false;
   Ray ray = new Ray(camera.transform.position, camera.transform.forward);
   RaycastHit hit;
   //Debug.DrawRay(ray.origin,ray.direction);
@@ -78,6 +113,37 @@ private void Start()
    {
     hit.transform.gameObject.GetComponent<PhotonView>().RPC("TakeDamage",RpcTarget.All,damage);
    }
+  }
+ }
+
+ void Recoil()
+ {
+  Vector3 finalPosition =
+   new Vector3(originalPosition.x, originalPosition.y + recoilUp, originalPosition.z - recoilBack);
+
+  transform.localPosition =
+   Vector3.SmoothDamp(transform.localPosition, finalPosition, ref recoilVelocity, recoilLength);
+
+  if (transform.localPosition==finalPosition)
+  {
+   recoiling = false;
+   recovering = true;
+  }
+ }
+ 
+ 
+ void Recover()
+ {
+  Vector3 finalPosition = originalPosition;
+   
+
+  transform.localPosition =
+   Vector3.SmoothDamp(transform.localPosition, finalPosition, ref recoilVelocity, recoverLength);
+
+  if (transform.localPosition==finalPosition)
+  {
+   recoiling = false;
+   recovering = false;
   }
  }
 }
